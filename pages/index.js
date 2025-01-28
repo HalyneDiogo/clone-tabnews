80c25eded1e412530c043d30f3e28ee03ad98965
Halyne Diogo <halyneteen@gmail.com>
2025-01-28 01:02:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-function Home () {
-    return(
-        <h1>Uhuuuu minha primeira hospedagem de um site. <br></br>
-Olá, Ana se tu acha que a Halyne é uma dev ruim dê uma risadinha!</h1>
-    )
-};
+function Home() {
+  return (
+    <h1>
+      Uhuuuu minha primeira hospedagem de um site. <br></br>
+      Olá, Ana se tu acha que a Halyne é uma dev ruim dê uma risadinha!
+    </h1>
+  );
+}
 
 export default Home;
