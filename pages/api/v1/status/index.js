3898dd0277c990983d53069a86116f3bd10bd50b
Halyne Diogo <halyneteen@gmail.com>
2025-02-11 19:29:15 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,38 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as soma ");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const dataVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseCurrentConnectionsResult = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseCurrentConnectionsValue = parseInt(
+    databaseCurrentConnectionsResult.rows[0].count,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataVersionValue,
+        max_connections: dataMaxConnectionsValue,
+        current_connections: databaseCurrentConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
